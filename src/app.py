aa7023bb8898a823f22c1b5e22b8dc9775f5cdb6
Daniel Vargas <danielv464@gmail.com>
2025-09-23 09:00:17 -0300

    Update and code cleaning on labeler

---
+++ src/app.py
@@ -0,0 +1,1 @@
+### Here I will build the tkinter app
--- src/labeler/labeler.py
+++ src/labeler/labeler.py
@@ -22,50 +22,35 @@ class ImageInput:
 class PatchBuilder:
     img: ImageInput
     patch_dict: dict[str, str] = field(init=False)
+    square_area: int = 64
 
-    def build_patches(self, square_area: int):
-        #print(self.img.x_length)
-        #print(self.img.y_length / 64)
+    
+    def build_dict(self):
 
-        #iterator_y = count(start=0, step=square_area)
-        #iterator_x = count(start=0, step=square_area)
-
-        #print(self.img.x_length)
-        x_stopper = round(self.img.x_length / square_area)
-        y_stopper = round(self.img.y_length / square_area)
-
-        #print(y_stopper)
-
-        for x in range(0, self.img.x_length, 64):
-            #print(y)
-            for y in range(64, self.img.y_length, 64):
-                print(y, x)
-
-    def building_coordinates(self, square_area):
-
-        x_stopper = round(self.img.x_length / square_area)
-        y_stopper = round(self.img.y_length / square_area)
+        x_stopper = round(self.img.x_length / self.square_area)
+        y_stopper = round(self.img.y_length / self.square_area)
 
         self.patch_dict = {
             f"row_{k + 1}": 
             {
-                f"col_{jdx + 1}": None for jdx in range(0, round(self.img.x_length / square_area))
+                f"col_{jdx + 1}": None for jdx in range(0, round(x_stopper))
             } for k in 
-            range(0, round(self.img.y_length / square_area))
+            range(0, round(y_stopper))
         }
 
-        #print(self.patch_dict)
+        return self.patch_dict
+        
 
+    def build_patches(self):
 
+        self.patch_dict = self.build_dict()
 
-        for idx, (y, y_prima) in enumerate(zip(range(0, self.img.y_length + square_area, square_area), 
-                              range(square_area, self.img.y_length + square_area, square_area))):
+        for idx, (y, y_prima) in enumerate(zip(range(0, self.img.y_length + self.square_area, self.square_area), 
+                              range(self.square_area, self.img.y_length + self.square_area, self.square_area))):
             
 
-            for jdx, (x, x_prima) in enumerate(zip(range(0, self.img.x_length + square_area, square_area), 
-                              range(square_area, self.img.x_length + square_area, square_area))):
-            
-                #print(f"y: {y, y_prima} y x: {x, x_prima}")
+            for jdx, (x, x_prima) in enumerate(zip(range(0, self.img.x_length + self.square_area, self.square_area), 
+                              range(self.square_area, self.img.x_length + self.square_area, self.square_area))):
 
                 # Save dictionaries key, value pairs.
                 self.patch_dict[f"row_{idx + 1}"][f"col_{jdx + 1}"] = {
@@ -75,32 +60,21 @@ class PatchBuilder:
                 
         return self.patch_dict
                 
-    def draw_squares(self, square_area: int = 64, set_number: int = None):
+    def draw_squares(self):
 
-        path_dict = self.building_coordinates(square_area=square_area)
+        patches = self.build_patches()
         
-        for row in path_dict.keys():
-            for col in path_dict[row]:
+        for row in patches.keys():
+            for col in patches[row]:
                 cv2.rectangle(self.img.img, 
-                              (path_dict[row][col]["x"][0], path_dict[row][col]["y"][0]),
-                              (path_dict[row][col]["x"][1], path_dict[row][col]["y"][1]),
+                              (patches[row][col]["x"][0], patches[row][col]["y"][0]),
+                              (patches[row][col]["x"][1], patches[row][col]["y"][1]),
                               (255, 0, 0),
                               4)
                 
         cv2.imshow('Image', self.img.img)
-        cv2.waitKey(0) # Waits indefinitely until a key is pressed.
-        cv2.destroyAllWindows() # Closes all windows opened by program.
-
-
-
-
-        
-
-
-        
-
-
-
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
@@ -108,7 +82,7 @@ if __name__ == "__main__":
     img_path = "/Users/danielvargas/Documents/wally/original-images/1.jpg"
 
     image = ImageInput(img_path=img_path)
-    pb = PatchBuilder(img=image)
+    pb = PatchBuilder(img=image, square_area=64)
 
     print("Size de la imagen:", image.size)
 
@@ -117,7 +91,7 @@ if __name__ == "__main__":
 
     #pb.build_patches(square_area=64)
 
-    pb.draw_squares(square_area=28)
+    pb.draw_squares()
 
     #pb.building_coordinates(square_area=128)
     #print("Size de la imagen:", image.size)
